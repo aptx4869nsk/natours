3105d9593ba9a91053a1f54059f8494c230a0d48
Nang San Kham <nangsanthinzarlwin@gmail.com>
2021-12-23 16:11:29 +0800

    Implemented CORS

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-const compression = require('compression');
 const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
@@ -8,6 +7,8 @@ const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
+const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -29,6 +30,16 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public'))); // Serving Static File
 
 // 1) GLOBAL MIDDLEWARES
+// Enable CORS
+app.use(cors());
+// Access-Control-Allow-Origin *
+// api.natours.com, natours.com
+// app.use(cors({
+//   origin: 'https://www.natours.com'
+// }))
+app.options('*', cors());
+// app.options('/api/v1/tous/:id', cors());  / preflight phase
+
 // Set security HTTP headers
 // app.use(helmet());
 app.use(
